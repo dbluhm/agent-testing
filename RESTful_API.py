71a17429c321b1cb22cbb5ecfb528cf426d14274
PeterStrob <peter@indicio.tech>
2021-08-26 12:25:40 -0700

    fixed docstring and list index problems

--- RESTful_API.py
+++ RESTful_API.py
@@ -7,13 +7,13 @@ from pydantic import (
     BaseModel,
 )
 
-""" Turns FastAPI into a function that we can pull commands from."""
+# Turns FastAPI into a function that we can pull commands from.
 app = (
     FastAPI()
 )
 
 
-""" Make an item class for the API to work with."""
+# Make an item class for the API to work with.
 
 
 class Item(BaseModel):
@@ -21,7 +21,7 @@ class Item(BaseModel):
     item_characteristics: str
 
 
-""" Make a list of items to work with."""
+# Make a list of items to work with.
 
 
 my_list = [
@@ -29,45 +29,45 @@ my_list = [
 ]
 
 
-"""Now we have a list to work with, let's make our necessary endpoints
-that work with it."""
-
-
-""" Adds a new item to the list.
-
-
-This function allows the user to specify details of an item
-they would want added to the list, adds those items to the
-list, and then returns the list's most recent addition. """
+# Now we have a list to work with, let's make
+# our necessary endpoints that work with it.
 
 
 @app.post("/new-item")
 def new_item(item: Item):
+
+    """ Adds a new item to the list.
+
+
+    This function allows the user to specify details of an item
+    they would want added to the list, adds those items to the
+    list, and then returns the list's most recent addition. 
+    """
     my_list.append(item)
     return my_list[-1]
 
 
-""" Returns the list and the items in it."""
-
-
 @app.get("/get-list")
 def get_list():
+    """ Returns the list and the items in it.
+    """
+
     return my_list
-
-
-""" Deletes a given item from a list.
-
-
-This function deletes an item with the specified index from
-the list. If the item index is not in the list, the user is
-told so. If it does, the corresponding item is deleted from
-the list and the user is told so. """
 
 
 @app.delete("/delete/{index}")
 def delete_item(item_num):
-    if item_num not in my_list:
+
+    """ Deletes a given item from a list.
+
+
+    This function deletes an item with the specified index from
+    the list. If the item index is not in the list, the user is
+    told so. If it does, the corresponding item is deleted from
+    the list and the user is told so. 
+    """
+    if item_num > len(my_list):
         return {"Error": "Item does not exist."}
 
-    del my_list[item_num]
+    del my_list[item_num-1]
     return {"Success": "Item deleted."}
